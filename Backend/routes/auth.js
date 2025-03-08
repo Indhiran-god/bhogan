808941b41814bf5f7c779dc4084847635b116e06
Indhiran-god <110456933+Indhiran-god@users.noreply.github.com>
2025-03-08 18:37:00 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -63,7 +63,7 @@ router.post("/createOrder", async (req, res) => {
   try {
 
     const options = {
-      amount: 100, // Razorpay uses paise
+      amount: 49900, // Razorpay uses paise
       currency: "INR",
       receipt: `order_rcptid_${Date.now()}`,
     };
@@ -154,3 +154,4 @@ router.post(
 );
 
 module.exports = router;
+
